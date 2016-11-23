18757f6da788617e69abe2327db9abcdf46b484b
Traci Williams <traciwilliams@Tracis-MacBook-Pro.local>
2016-11-23 13:02:01 -0600

    final without bathrooms

--- scripts/app.js
+++ scripts/app.js
@@ -6,74 +6,74 @@
         {
               locationName: "chicagoMap",
               geolocation: {lat: 41.88425, lng: -87.63245},
-              comments: "Chicago does this work"
+              comments: "<h3>Chicago</h3>"
         },
 
         {
               locationName: "greektown",
               geolocation: {lat: 41.8786, lng: -87.6471},
-              comments: "Greek does this work"
+              comments: "<h3>Greektown</h3>"
         },
         {
               locationName: "goldcoast",
               geolocation: {lat: 41.9058, lng: -87.6273},
-              comments: "Gold does this work"
+              comments: "<h3>Goldcoast</h3>"
         },
         {
               locationName: "hydepark",
               geolocation: {lat: 41.7943, lng: -87.5907},
-              comments: "<h2><font color: red>Hyde Park</font>  </h2> does this work"
+              comments: "<h3>Hyde Park</h3>"
         },
         {
               locationName: "printersrow",
               geolocation: {lat:41.8783, lng: -87.6273},
-              comments: "Printers does this work"
+              comments: "<h3>Printer's Row</h3>"
         },
 
           {
               locationName: "rivernorth",
               geolocation: {lat: 41.8924, lng: -87.6341},
-              comments: "River Northdoes this work"
+              comments: "<h3>River North</h3>"
           },
          {
               locationName: "wrigleyville",
               geolocation: {lat:41.948437, lng: -87.655334},
-              comments: "Wrigleyville does this work"
+              comments: "<h3>Wrigleyville</h3>"
          },
         {
               locationName: "wickerpark",
               geolocation: {lat:41.9088, lng: -87.6796},
-              comments: "Wicker Park does this work"
+              comments: "<h3>Wicker Park</h3>"
         },
         {
               locationName: "oldtown",
               geolocation: {lat:41.9077, lng: -87.6374},
-              comments: "Old Town does this work"
+              comments: "<h3>Old Town</h3>"
         },
         {
               locationName: "westloop",
               geolocation: {lat:41.8825, lng: -87.6447},
-              comments: "West Loop does this work"
+              comments: "<h3>West Loop</h3>"
         },
         {
               locationName: "magmile",
               geolocation: {lat:41.8948, lng: -87.6242},
-              comments: "Mag Mile does this work"
+              comments: "<h3>Mag Mile</h3>"
         },
         {
               locationName: "lincolnpark",
               geolocation: {lat:41.9214, lng: -87.6513},
-              comments: "Lincoln Park does this work"
+              comments: "<h3>Lincoln Park</h3>"
         },
         {
               locationName: "lakeview",
               geolocation: {lat:41.9436, lng: -87.6584},
-              comments: "Lake View does this work"
+              comments: "<h3>Lakeview</h3>"
         },
         {
               locationName: "loop",
               geolocation: {lat: 41.8837, lng: -87.6289},
-              comments: "Loop does this work"
+              comments: "<h3>Loop</h3>"
         }
 
       ];
@@ -82,7 +82,6 @@
 
    };
         
-
 
 
 // ============FORM AUTOCOMPLETE===============-->
@@ -101,6 +100,17 @@
             {types: ['geocode']});
         autocomplete.addListener('place_changed', fillInAddress);
       }
+
+
+
+        // $scope.initAutocomplete = function(){
+        //     autocomplete = new google.maps.places.Autocomplete(
+        //     /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
+        //     {types: ['geocode']});
+        //     autocomplete.addListener('place_changed', fillInAddress);
+
+        //     // $scope.initAutocomplete()
+        // }
 
 
       //getting info from autocomplete
@@ -140,7 +150,8 @@
               
               var geolocation = {
                   lat: position.coords.latitude,
-                  lng: position.coords.longitude
+                  lng: position.coords.longitude,
+                  //zoom: 5
               };
               console.log(geolocation)
               var circle = new google.maps.Circle({
@@ -149,41 +160,31 @@
               });
               autocomplete.setBounds(circle.getBounds());
 
-
               var pottymap = new google.maps.Map(document.getElementById('map'),
                   {
                       center: geolocation,
-                      zoom: 16
-                  });
-
-
-
-
+                      //zoom: 16
+                      zoom: 12
+                      });
+                
               for (i = 0; i < locationsArray.length; i++) {
                   var locations = locationsArray[i].geolocation; //this might not be right
                    
                   var marker = new google.maps.Marker({
                       position: locations,
-                      //animation: google.maps.Animation.BOUNCE, //bounce animation
-                      
+                      animation: google.maps.Animation.BOUNCE, //bounce animation
+                      //animation: google.maps.Animation.DROP,
+                      //icon: "icon.jpg",
                       map: pottymap
                     })
-                    
-                  
-
-
 
                    var infowindow = new google.maps.InfoWindow ({ 
                         maxWidth:200,
                        //pixelOffset: new google.maps.Size(200,200),
                        content: locationsArray[i].comments
                    });
-                   
-                   
-
 
                    add(marker, infowindow, pottymap, infowindow.content)
-
              
               }
           })
@@ -199,30 +200,26 @@ function add(marker, infoWindow, map, comments){
                    });
  }           
 
-// var iconBase = 'icon.jpg';
-//   var marker = new google.maps.Marker({
-//     position: myLatLng,
-//     map: map,
-//     icon: iconBase + 'potty'
-//   });
 
 
+    // function bounceStop(){
+    //   setTimeout(function(){
+    //     marker.Animation();
+    //   }, 3000);
+    // };
+
+                
+
+// marker.addListener('click', toBounce);
+                  
+
+// function toBounce() {
+//   if (marker.getAnimation() !== null) {
+//     marker.setAnimation(nulll);
+//   } else {
+//     marker.setAnimation(google.maps.Animation.BOUNCE);
+//   }
+// }
 
 
-
-
-
-
-//=============possible code to make infoWindow work===========
-
-
-   //
-   //
-   // var marker = new google.maps.Marker({
-   //     position: locations,
-   //     map: pottymap
-   // });
-   //
-  
-  // ==================================================================
 
